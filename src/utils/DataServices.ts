5f6951c444a258c3cf20df2e7d2518174483a21b
BrockSpacek <brockspacek1@gmail.com>
2025-03-27 11:35:43 -0700

    Continued Front Logic

---
+++ src/utils/DataServices.ts
@@ -0,0 +1,55 @@
+import { IUserData, IUserInfo } from "./Interfaces"
+
+
+const url = "https://spacekbblog-dzhvdueagzdha7cx.westus-01.azurewebsites.net/"
+
+// This variable will be used in our getblog by user id fetch when we set them up
+
+let userData: IUserData
+
+// Create Account fetch
+export const createAccount = async (user:IUserInfo) => {
+    const res = await fetch(url + "User/CreateUser",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(user)
+    });
+    // If our response is Not ok we will run this block
+    if(!res.ok){
+        const data = await res.json();
+        const message = data.message;
+        console.log(message);
+        return data.success;
+    }
+
+    const data = await res.json();
+    return data.success;
+}
+
+// Login Fetch
+
+export const login = async (user:IUserInfo) => {
+    const res = await fetch(url + "User/Login",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(user)
+    });
+
+    if(!res.ok){
+        const data = await res.json();
+        const message = data.message;
+        console.log(message);
+        return null;
+    }
+
+    const data = await res.json();
+    return data;
+}
+
+// Get Logged Data Fetch
+
+//
---
+++ src/utils/Interfaces.ts
@@ -0,0 +1,28 @@
+export interface IBlogItems {
+    id: number
+    userId: number
+    publisherName: string
+    date: string
+    title: string
+    image: string
+    description: string
+    category: string
+    isPublished: boolean
+    isDeleted: boolean
+}
+// This will be used for our Login and Create Account fetch
+export interface IUserInfo {
+    username: string
+    password: string  
+}
+
+// This will be used to fetch our user data id and name
+export interface IUserData {
+    id: number
+    username: string
+}
+
+//Interface for getting our Token
+export interface Itoken {
+    token: string
+}
